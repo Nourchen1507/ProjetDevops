b2bb5eb4954fb06eb57db2c9c20ea60bbc7dee24
ilyes-gtari <ilyes.gtari@esprit.tn>
2023-10-04 17:56:19 +0200

    test de modification

--- src/main/java/tn/esprit/rh/achat/entities/Facture.java
+++ src/main/java/tn/esprit/rh/achat/entities/Facture.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Facture implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+// testttttttttttttttt !!!!!!!!! ilyes
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idFacture;

0e8c2a912d82d0efc809f3282a4bb1cac964c693
ilyes-gtari <ilyes.gtari@esprit.tn>
2023-11-04 03:24:45 +0100

    tet

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -40,9 +40,9 @@ p
 @src/main/java/tn/esprit/rh/achat/services/IOperateurService.java,2\9\294617bd11cdd2595aea33d92c998d9eedc7422c
 s
 Csrc/main/java/tn/esprit/rh/achat/services/OperateurServiceImpl.java,0\b\0b234be7fa057b8ddba600b797354d753b09916f
-w
-Gsrc/test/java/tn/esprit/rh/achat/services/OperateurServiceImplTest.java,b\e\bef08db45a43dceeb1db41bd468c45c2a87094cc
 h
 8src/main/java/tn/esprit/rh/achat/entities/Operateur.java,a\5\a5a347794845806e14fb656ee63dba3468d85c59
 w
 Gsrc/main/java/tn/esprit/rh/achat/controllers/ProduitRestController.java,4\4\44049d0dceb7e63b9cff4011ee7654aee115e79c
+s
+Csrc/test/java/tn/esprit/rh/achat/services/OperateurServiceTest.java,c\1\c155b2bad9e2a93e09a36dd05470b8a327389580
--- src/test/java/tn/esprit/rh/achat/services/OperateurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/OperateurServiceTest.java
@@ -1,74 +1,74 @@
-package tn.esprit.rh.achat.services;
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import tn.esprit.rh.achat.entities.Operateur;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@Transactional
-@ActiveProfiles("test")
-public class OperateurServiceTest {
-
-    @Autowired
-    private IOperateurService operateurService;
-
-    @Test
-    void retrieveAllOperateurs() {
-        // Testez la récupération de tous les opérateurs et vérifiez si la liste n'est pas nulle.
-        List<Operateur> operateurs = operateurService.retrieveAllOperateurs();
-        assertNotNull(operateurs);
-        // Effectuez d'autres assertions selon votre logique métier.
-    }
-
-    @Test
-    void addOperateur() {
-        // Testez l'ajout d'un opérateur et vérifiez si l'opérateur est correctement enregistré.
-        Operateur nouvelOperateur = new Operateur();
-        nouvelOperateur.setIdOperateur(10L);
-        nouvelOperateur.setNom("gtari");
-        Operateur operateurEnregistre = operateurService.addOperateur(nouvelOperateur);
-        assertNotNull(operateurEnregistre);
-        // Effectuez d'autres assertions selon votre logique métier.
-    }
-
+//package tn.esprit.rh.achat.services;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//import tn.esprit.rh.achat.entities.Operateur;
+//
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+//@Transactional
+//@ActiveProfiles("test")
+//public class OperateurServiceTest {
+//
+//    @Autowired
+//    private IOperateurService operateurService;
+//
 //    @Test
-//    void retrieveOperateur() {
-//        // Testez la récupération d'un opérateur par ID.
-//        Long idOperateur = 10L; // Remplacez par un ID valide.
-//        Operateur operateurRecupere = operateurService.retrieveOperateur(idOperateur);
-//        assertNotNull(operateurRecupere);
+//    void retrieveAllOperateurs() {
+//        // Testez la récupération de tous les opérateurs et vérifiez si la liste n'est pas nulle.
+//        List<Operateur> operateurs = operateurService.retrieveAllOperateurs();
+//        assertNotNull(operateurs);
 //        // Effectuez d'autres assertions selon votre logique métier.
 //    }
 //
 //    @Test
-//    void updateOperateur() {
-//        // Testez la mise à jour d'un opérateur.
-//        Long idOperateur = 10L; // Remplacez par un ID valide.
-//        Operateur operateurToUpdate = operateurService.retrieveOperateur(idOperateur);
-//        assertNotNull(operateurToUpdate);
-//        // Modifiez les propriétés de l'opérateur si nécessaire.
-//        operateurToUpdate.setNom("Nouveau Nom");
-//        Operateur operateurMiseAJour = operateurService.updateOperateur(operateurToUpdate);
-//        assertNotNull(operateurMiseAJour);
+//    void addOperateur() {
+//        // Testez l'ajout d'un opérateur et vérifiez si l'opérateur est correctement enregistré.
+//        Operateur nouvelOperateur = new Operateur();
+//        nouvelOperateur.setIdOperateur(10L);
+//        nouvelOperateur.setNom("gtari");
+//        Operateur operateurEnregistre = operateurService.addOperateur(nouvelOperateur);
+//        assertNotNull(operateurEnregistre);
 //        // Effectuez d'autres assertions selon votre logique métier.
 //    }
 //
-//    @Test
-//    void deleteOperateur() {
-//        // Testez la suppression d'un opérateur par ID.
-//        Long idOperateur = 2L; // Remplacez par un ID valide.
-//        operateurService.deleteOperateur(idOperateur);
-//        Operateur operateurSupprime = operateurService.retrieveOperateur(idOperateur);
-//        assertNull(operateurSupprime);
-//    }
-}
+////    @Test
+////    void retrieveOperateur() {
+////        // Testez la récupération d'un opérateur par ID.
+////        Long idOperateur = 10L; // Remplacez par un ID valide.
+////        Operateur operateurRecupere = operateurService.retrieveOperateur(idOperateur);
+////        assertNotNull(operateurRecupere);
+////        // Effectuez d'autres assertions selon votre logique métier.
+////    }
+////
+////    @Test
+////    void updateOperateur() {
+////        // Testez la mise à jour d'un opérateur.
+////        Long idOperateur = 10L; // Remplacez par un ID valide.
+////        Operateur operateurToUpdate = operateurService.retrieveOperateur(idOperateur);
+////        assertNotNull(operateurToUpdate);
+////        // Modifiez les propriétés de l'opérateur si nécessaire.
+////        operateurToUpdate.setNom("Nouveau Nom");
+////        Operateur operateurMiseAJour = operateurService.updateOperateur(operateurToUpdate);
+////        assertNotNull(operateurMiseAJour);
+////        // Effectuez d'autres assertions selon votre logique métier.
+////    }
+////
+////    @Test
+////    void deleteOperateur() {
+////        // Testez la suppression d'un opérateur par ID.
+////        Long idOperateur = 2L; // Remplacez par un ID valide.
+////        operateurService.deleteOperateur(idOperateur);
+////        Operateur operateurSupprime = operateurService.retrieveOperateur(idOperateur);
+////        assertNull(operateurSupprime);
+////    }
+//}

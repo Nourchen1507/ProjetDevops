d7172915058640a76c2a75ea44ea740c3a565798
ilyes-gtari <ilyes.gtari@esprit.tn>
2023-11-04 04:19:07 +0100

    jj

--- src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceTest.java
@@ -1,34 +1,34 @@
-package tn.esprit.rh.achat.services;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import tn.esprit.rh.achat.entities.CategorieProduit;
-
-import java.util.List;
-
-@SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
-public class CategorieProduitServiceTest {
-
-    @Autowired
-    private ICategorieProduitService categorieProduitService;
-
-    @Test
-    @Order(1)
-    public void testRetrieveAllCategorieProduits() {
-        List<CategorieProduit> listCategorieProduits = categorieProduitService.retrieveAllCategorieProduits();
-        Assertions.assertEquals(3, listCategorieProduits.size());
-    }
-
-
-
-
-
-
-}
-
+//package tn.esprit.rh.achat.services;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+//import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestMethodOrder;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import tn.esprit.rh.achat.entities.CategorieProduit;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//@TestMethodOrder(OrderAnnotation.class)
+//public class CategorieProduitServiceTest {
+//
+//    @Autowired
+//    private ICategorieProduitService categorieProduitService;
+//
+//    @Test
+//    @Order(1)
+//    public void testRetrieveAllCategorieProduits() {
+//        List<CategorieProduit> listCategorieProduits = categorieProduitService.retrieveAllCategorieProduits();
+//        Assertions.assertEquals(3, listCategorieProduits.size());
+//    }
+//
+//
+//
+//
+//
+//
+//}
+//
